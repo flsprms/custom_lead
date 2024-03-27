cb6585ece114ce5fdcd1c98c38ed2d939b5e5694
flsprms <149524130+flsprms@users.noreply.github.com>
2024-03-27 19:43:18 +0300

    Delete employee directory

--- employee/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Custom hr.employee',
-    'author': 'Alexandrov Danil',
-    'summary': 'Odoo 17',
-    'depends': ['hr'],
-    'data': [
-        'views/employee.xml'
-    ]
-}
--- employee/models/employee.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class Employee(models.Model):
-    _inherit = 'hr.employee'
-
-    telegram_id = fields.Char(string='Telegram ID')
